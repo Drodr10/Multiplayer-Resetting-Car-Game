22ed2cdcc59fe6c01610652928395cf5b1c31a01
Drodr10 <diegor010206@gmail.com>
2020-09-16 15:35:12 -0400

    Made ball move throught different windows

--- sketch.js
+++ sketch.js
@@ -1,29 +1,46 @@
 var ball;
+var database, position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var info = database.ref("Ball/Position");
+    info.on("value", readPos, err);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("Ball/Position").set(
+        {
+            x: ball.x + x,
+            y: ball.y + y
+        })
+}
+
+function readPos(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function err(){
+    console.log("There was an error while reading database");
 }
